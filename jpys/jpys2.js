87ad7bb61dc43bf33f9479290e2a85017fe80668
Zephyr <wang_zi_mo@163.com>
2025-09-30 14:51:43 +0800

    Update jpys2.js

--- jpys/jpys2.js
+++ jpys/jpys2.js
@@ -3,40 +3,39 @@ async function searchResults(keyword) {
 
     try {
 		// 获取搜索结果总页数
-    	const encodedKeyword = encodeURIComponent(keyword);
+        const encodedKeyword = encodeURIComponent(keyword);
+        const searchUrl = `https://hnytxj.com/vod/search/${encodedKeyword}?_rsc=xsbs6`;
 		const headers = {
-		'RSC': '1'
+		    'RSC': '1'
         };
-        const response = await fetchv2(`https://hnytxj.com/vod/search/${encodedKeyword}?_rsc=xsbs6`, { headers });
+        const response = await fetch(url, headers);
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-
         	// 根据实际返回内容类型选择解析方式
         const contentType = response.headers.get('content-type');
         let data;
-
         if (contentType && contentType.includes('application/json')) {
             data = await response.json();
         } else {
             data = await response.text();
         }
-		console.log(`返回内容： ${data}， --- :${JSON.stringify(data)}`);
+		console.log(`返回内容： ${data}, --- :${JSON.stringify(data)}`);
 		throw new Error(`返回内容：${data} `);
-
-        return JSON.stringify([{ title: 'NULL', image: '', href: '' }]);;
+		return JSON.stringify([{ title: 'NULL', image: '', href: '' }]);
     } catch (error) {
         console.error(`❌ 搜索失败:`, error);
-        return JSON.stringify([{ title: 'Error', image: '', href: '' }]);;
+        return JSON.stringify([{ title: 'Error', image: '', href: '' }]);
+
     }
 }
-
 async function extractDetails(url) {
 	return JSON.stringify({
-            description: 'Error loading description',
+            description: 'NULL',
             aliases: 'Duration: Unknown',
             airdate: 'Aired: Unknown'
+        });
 }
 
 async function extractEpisodes(url) {
@@ -45,7 +44,7 @@ async function extractEpisodes(url) {
 }
 
 async function extractStreamUrl(url) {
-	rreturn JSON.stringify({ streams: [] });
+	return JSON.stringify({ streams: [] });
 }
 
 // searchResults("战").then(console.log);
